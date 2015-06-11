102597d15fa6ac096896675709750c251d52f36d
Wusung <twkoci@gmail.com>
2015-06-11 22:20:35 +0800

    Remove unused files

--- bash/start-notebook.sh
+++
@@ -1,3 +0,0 @@
-cd ~/workspace/kyper-core/docker-builds/flyberry/
-export IPYTHONDIR=~/workspace/kyper-core/docker-builds/flyberry/ipython-frontend
-ipython notebook --port=8888 --profile=nbserver --log-level=DEBUG
