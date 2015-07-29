510097c43917650ea110f5d42a9e0604801f858f
Wusung Peng <wusung.peng@kyerpdata.com>
2015-07-29 17:26:57 +0800

    Add jsonlint to generate formatted json doc

--- bin/weather.sh
+++ bin/weather.sh
@@ -17,19 +17,24 @@ if [ ! -d "${APP_DIR}" ]; then
   APP_DIR=`cd "${APP_DIR}"; pwd`
 fi
 
-echo $APP_DIR
-executable="${APP_DIR}/lib/codegenerator-1.0-SNAPSHOT.jar"
-executable="${APP_DIR}/build/libs/codegenerator-1.0-SNAPSHOT.jar"
+echo $APP_DIR=./
+#executable="${APP_DIR}/lib/codegenerator-1.0-SNAPSHOT.jar"
+#executable="${APP_DIR}/build/libs/codegenerator-1.0-SNAPSHOT.jar"
+executable="./build/libs/codegenerator-1.0-SNAPSHOT.jar"
 
 if [ ! -f "$executable" ]
 then
   mvn clean package
 fi
 
+APP_DIR=./
 LIB_PATH=${APP_DIR}/build/libs/*:../build/libs/*:${APP_DIR}/libs/*
 
 # if you've executed sbt assembly previously it will use that instead.
 export JAVA_OPTS="${JAVA_OPTS} -XX:MaxPermSize=256M -Xmx1024M -DloggerPath=conf/log4j.properties -cp ${LIB_PATH}"
 ags="$@ generate -t template -i ${APP_DIR}/conf/weather.yml -l com.kyper.MyPythonCodeGenerator -o ${APP_DIR}/output"
 
+#/c/Java/jdk1.7.0_80/bin/java $JAVA_OPTS -jar $executable $ags       
 java $JAVA_OPTS -jar $executable $ags       
+
+jsonlint -o doc/weather.json -f output/SwaggerPetstore-python/SwaggerPetstore/apis/default_api.json 
